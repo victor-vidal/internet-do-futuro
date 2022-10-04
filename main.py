c78322482fa7a42cc8f1da93bf8ae3ab4d07d993
AlcidesAndrade <54755722+AlcidesAndrade@users.noreply.github.com>
2022-10-03 23:33:44 -0300

    remove unused "PACKAGE_COUNT"

--- main.py
+++ main.py
@@ -5,25 +5,24 @@ import sys
 # ref: https://stackoverflow.com/questions/2953462/pinging-servers-in-python
 
 if sys.argv[1] == 'master':
-    PACKAGE_COUNT = 62
+    #PACKAGE_COUNT = 62
     PACKAGE_HEADER_SIZE = 28
     PACKAGE_SIZE = 77 - PACKAGE_HEADER_SIZE
     PACKAGE_INTERVAL = 1
-    HOST = '127.0.0.1'
     
     print("Master script\n")
     command = ""
     for host in sys.argv[2:]:
-        command += f"ping -s {PACKAGE_SIZE} -c {PACKAGE_COUNT} -i {PACKAGE_INTERVAL} {host} & "
+        command += f"ping -s {PACKAGE_SIZE} -i {PACKAGE_INTERVAL} {host} & "
     
     os.system(command)
     print("Terminou\n")
 else:
-    PACKAGE_COUNT = 31
+    #PACKAGE_COUNT = 31
     PACKAGE_HEADER_SIZE = 28
     PACKAGE_SIZE = 88 - PACKAGE_HEADER_SIZE
     PACKAGE_INTERVAL = 2
     
     print("Slave script\n")
-    os.system(f"ping -s {PACKAGE_SIZE} -c {PACKAGE_COUNT} -i {PACKAGE_INTERVAL} {sys.argv[2]}")
+    os.system(f"ping -s {PACKAGE_SIZE} -i {PACKAGE_INTERVAL} {sys.argv[2]}")
     print("\nTerminou\n")
